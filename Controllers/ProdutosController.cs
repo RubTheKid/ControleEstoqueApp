dc6213c205ec64a528a353bcdf3996fbf982becf
jader.cardoso <jader.cardoso@TPCP06INF026230>
2023-03-03 16:47:47 -0300

    Add project files.

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ControleEstoqueApp.Models;
+using System.Text;
+
+namespace ControleEstoqueApp.Controllers;
+
+public class ProdutosController : Controller
+{
+    private readonly string apiUrl = "https://localhost:44320/api/Produtos";
+
+    //GET
+    public async Task<ActionResult> Index()
+    {
+        List<Produto> ListaProdutos = new List<Produto>();
+
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.GetAsync(apiUrl))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(apiResponse);
+                ListaProdutos = JsonConvert.DeserializeObject<List<Produto>>(apiResponse);
+            }
+        }
+        return View(ListaProdutos);
+    }
+
+    //GET{id}
+    public ViewResult GetProduto() => View();
+    [HttpPost]
+    public async Task<IActionResult> GetProduto(int id)
+    {
+        Produto produto = new Produto();
+
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.GetAsync(apiUrl + "/" + id))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                produto = JsonConvert.DeserializeObject<Produto>(apiResponse);
+            }
+
+        }
+        return View(produto);
+    }
+
+    //POST
+    public ViewResult AddProduto() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> AddProduto(Produto produto)
+    {
+        Produto produtoAdd = new Produto();
+
+        using (var httpClient = new HttpClient())
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(produto), Encoding.UTF8, "application/json");
+
+            using (var response = await httpClient.PostAsync(apiUrl, content))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(apiResponse);
+                Console.WriteLine(produto);
+                Console.WriteLine(content);
+
+                produtoAdd = JsonConvert.DeserializeObject<Produto>(apiResponse);
+            }
+        }
+        return View(produtoAdd);
+    }
+
+}
---
+++ Models/ProdutosModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ControleEstoqueApp.Models
+{
+
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+
+        public decimal? Preco { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+
+    }
+}
+
+//public class ProdutosViewModel
+//{ 
+//    public IEnumerable<Produto>? Produtos { get; set; }
+//}
