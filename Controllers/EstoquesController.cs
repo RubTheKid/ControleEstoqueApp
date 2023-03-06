501866f924bba4446bc09dd9b5cff6624b444ea9
RubTheKid <you@example.com>
2023-03-06 18:51:13 -0300

    Update: estoques creation

---
+++ Controllers/EstoquesController.cs
@@ -0,0 +1,29 @@
+﻿using ControleEstoqueApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ControleEstoqueApp.Controllers;
+
+public class EstoquesController : Controller
+{
+
+    private readonly string apiUrl = "https://localhost:44320/api/ItemEstoques";
+
+    //GET
+    public async Task<ActionResult> Index()
+    {
+        List<Estoque> ListaEstoques = new List<Estoque>();
+
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.GetAsync(apiUrl))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(apiResponse);
+                ListaEstoques = JsonConvert.DeserializeObject<List<Estoque>>(apiResponse);
+            }
+        }
+        return View(ListaEstoques);
+    }
+}
---
+++ Models/EstoquesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleEstoqueApp.Models
+{
+    public class Estoque
+    {
+        public int ItemEstoqueId { get; set; }
+        public string? Nome { get; set; }
+        public int ProdutoId { get; set; }
+        public int LojaId { get; set; }
+    }
+}
