6fc4c46d83c71d081e535663bd32b90584da34f5
RubTheKid <you@example.com>
2023-03-09 16:21:35 -0300

    Update: estoques views, fixes: EstoqueController, Lojas Views, ProdutosViews

--- Controllers/EstoquesController.cs
+++ Controllers/EstoquesController.cs
@@ -19,7 +19,6 @@ public class EstoquesController : Controller
             using (var response = await httpClient.GetAsync(apiUrl))
             {
                 string apiResponse = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(apiResponse);
                 ListaEstoques = JsonConvert.DeserializeObject<List<ItemEstoques>>(apiResponse);
             }
         }
@@ -46,4 +45,85 @@ public class EstoquesController : Controller
         }
         return View(estoque);
     }
+
+    //POST
+    public ViewResult AddEstoque() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> AddEstoque(ItemEstoques estoque)
+    {
+        ItemEstoques estoqueAdd = new ItemEstoques();
+
+        using (var httpClient = new HttpClient())
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(estoque), Encoding.UTF8, "application/json");
+
+            using (var response = await httpClient.PostAsync(apiUrl, content))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+
+                estoqueAdd = JsonConvert.DeserializeObject<ItemEstoques>(apiResponse);
+            }
+        }
+        return View(estoqueAdd);
+    }
+
+    //PUT
+
+
+
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateEstoque(int id)
+    {
+        ItemEstoques estoque = new ItemEstoques();
+
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.GetAsync(apiUrl + "/" + id))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                estoque = JsonConvert.DeserializeObject<ItemEstoques>(apiResponse);
+            }
+        }
+        return View(estoque);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateEstoque(ItemEstoques estoque)
+    {
+        ItemEstoques UpdateEstoque = new ItemEstoques();
+
+        using (var httpClient = new HttpClient())
+        {
+
+            var content = JsonContent.Create<ItemEstoques>(estoque);
+
+            using (var response = await httpClient.PutAsync(apiUrl, content))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                ViewBag.Result = "Sucesso";
+                UpdateEstoque = JsonConvert.DeserializeObject<ItemEstoques>(apiResponse);
+            }
+        }
+
+        return View(UpdateEstoque);
+    }
+
+
+    //DELETE
+    [HttpPost]
+    public async Task<IActionResult> DeleteEstoque(int Id)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.DeleteAsync(apiUrl + "/" + Id))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+            }
+        }
+        return RedirectToAction("Index");
+    }
+
 }
